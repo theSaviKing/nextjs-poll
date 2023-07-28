906e7cfe8c61e662f15a338ef79dc3a9857b1e0a
T. Paul Kariuki <tpaulkariuki@gmail.com>
2023-07-28 11:45:12 -0500

    idk, some more stuff

--- src/app/polls/[poll]/page.tsx
+++ src/app/polls/[poll]/page.tsx
@@ -13,8 +13,6 @@ export async function generateMetadata({
     };
 }
 
-// trying to get Vercel to deploy...
-
 export default async function Poll({ params }: { params: { poll: string } }) {
     const pollId = Number(params.poll);
     // console.log("pollId = ", pollId, typeof pollId);
